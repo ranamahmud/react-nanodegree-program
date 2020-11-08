06aeb2a17e2035f3c2e3d91f8a9d43829100f8c3
Md Rana Mahmud <ranamd0@gmail.com>
2020-11-08 19:10:54 +0600

    history connected

--- 5. React Native/UdaciFitness/components/History.js
+++ 5. React Native/UdaciFitness/components/History.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
-export default class History extends Component {
+import { connect } from 'react-redux'
+import { receiveEntries, addEntry } from '../actions'
+import { timeToString, getDailyReminderValue } from '../utils/helpers'
+import { fetchCalendarResults } from '../utils/api'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+class History extends Component {
+    componentDidMount() {
+        const { dispatch } = this.props
+        fetchCalendarResults()
+            .then((entries) => dispatch(receiveEntries(entries)))
+            .then(({ entries }) => {
+                if (!entries[timeToString()]) {
+                    dispatch(addEntry({
+                        [timeToString()]: getDailyReminderValue()
+                    }))
+                }
+            })
+    }
     render() {
+        const { entries } = this.props;
         return (
             <View>
-                <Text> History </Text>
+                <UdaciFitnessCalendar
+                    items={entries}
+                    renderItem={ }
+                    renderEmptyDate={ } />
             </View>
         )
     }
 }
+
+function mapStateToProps(entries) {
+    return {
+        entries
+    }
+}
+export default connect(mapStateToProps)(History)
